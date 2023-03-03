58cea3461b2b4f0ed0e606976960a42b60f3b265
Jayavenkate <jayajaikithuja@gmail.com>
2023-03-03 18:44:24 +0300

    table box

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,6 @@ import { DataList } from './DataList';
 export function Card(){
     return(
         <div className="container">
-        {/* <h1 className="heading">Pricing table</h1> */}
         <DataList />
          </div>
         )}
